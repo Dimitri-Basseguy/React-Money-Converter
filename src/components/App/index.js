03c678e2023a5cd87edeaff2cad70ed8b5866130
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-07-29 14:23:03 +0200

    Ajout donnée dans le composant Currencies

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,14 +7,23 @@ import Currencies from 'src/components/Currencies';
 import Results from 'src/components/Results';
 import './app.scss';
 
+// Import Data
+import datas from 'src/data/currencies';
+
 // == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <Currencies />
-    <Results />
-  </div>
-);
+const App = () => {
+  console.log(datas);
+
+  return (
+    <div className="app">
+      <Header />
+      <Currencies
+        moneys={datas}
+      />
+      <Results />
+    </div>
+  );
+};
 
 // == Export
 export default App;
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,27 +1,25 @@
 // == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import
 import './currencies.scss';
 
-const Currencies = () => (
+const Currencies = ({ moneys }) => (
   <div className="currencie">
     <h2 className="currencie-title">Currencies</h2>
     <ul>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
-      <li className="currencie-money">sdfsdf</li>
+      {moneys.map((money) => (
+        <li key={money.name.toString()} className="currencie-money">{money.name}</li>
+      ))}
     </ul>
   </div>
 );
+
+// Validation des props
+// Validation des props
+Currencies.propTypes = {
+  moneys: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Currencies;
