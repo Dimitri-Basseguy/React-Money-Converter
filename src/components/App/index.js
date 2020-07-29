1354812e1b09fd57f6c5e2768400d504ed37a7ed
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-07-29 15:52:46 +0200

    composant transformer en class avec un -state-

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,24 +6,44 @@ import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Results from 'src/components/Results';
 import './app.scss';
-
 // Import Data
 import datas from 'src/data/currencies';
 
-// == Composant
-const App = () => {
-  console.log(datas);
+// pour gérer un state react (état de mon appli), 2 possibilités :
+// - transformer le compsant en class
+// - utiliser le hook useState
 
-  return (
-    <div className="app">
-      <Header />
-      <Currencies
-        moneys={datas}
-      />
-      <Results />
-    </div>
-  );
-};
+// parenthèse si JSX sur plusieur lignes
+// {open && (
+//   <div>
+//     <p>ligne 1</p>
+//   </div>
+// )}
+
+// Composant sous forme de class : render() retourne le JSX
+class App extends React.Component {
+  // Constructeur : On passe props en paramètre, même si on n'en a pas
+  constructor(props) {
+    // On appelle le constructeur de react.component
+    super(props);
+    // On crée le state de notre appli
+    this.state = {
+      open: false,
+    };
+  }
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div className="app">
+        <Header />
+        {open && <Currencies moneys={datas} />}
+        <Results />
+      </div>
+    );
+  }
+}
 
 // == Export
 export default App;
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -5,21 +5,32 @@ import PropTypes from 'prop-types';
 // == Import
 import './currencies.scss';
 
-const Currencies = ({ moneys }) => (
-  <div className="currencie">
-    <h2 className="currencie-title">Currencies</h2>
-    <ul>
-      {moneys.map((money) => (
-        <li key={money.name.toString()} className="currencie-money">{money.name}</li>
-      ))}
-    </ul>
-  </div>
-);
+const Currencies = ({ moneys }) => {
+  console.log(moneys);
+  return (
+    <div className="currencie">
+      <h2 className="currencie-title">Currencies</h2>
+      <ul>
+        {moneys.map((money) => (
+          <li key={money.name.toString()} className="currencie-money">{money.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 // Validation des props
-// Validation des props
 Currencies.propTypes = {
-  moneys: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // moins précis
+  // moneys: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+  // plus précis
+  moneys: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      rate: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
 
 export default Currencies;
